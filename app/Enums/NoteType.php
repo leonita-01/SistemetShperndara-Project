76d00de29dc781fe924ddc85e19556d8d7c1510d
SumejaV <156528965+SumejaV@users.noreply.github.com>
2025-05-22 16:24:55 +0200

    Update NoteType.php

--- app/Enums/NoteType.php
+++ app/Enums/NoteType.php
@@ -4,8 +4,10 @@ namespace App\Enums;
 
 enum NoteType: string
 {
+
     case Refund = 'refund';
     case Warranty = 'warranty';
     case Shipping = 'shipping';
     case Delivery = 'delivery';
+    
 }
